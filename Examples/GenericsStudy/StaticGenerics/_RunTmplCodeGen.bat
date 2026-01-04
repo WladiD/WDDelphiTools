c009e89a24c18740f0b8f20e9fc367d562d3912d
WladiD <furevest@gmail.com>
2026-01-04 08:49:50 +0100

    Benchmark_StaticGenerics compiles now

--- Examples/GenericsStudy/StaticGenerics/_RunTmplCodeGen.bat
+++ Examples/GenericsStudy/StaticGenerics/_RunTmplCodeGen.bat
@@ -3,16 +3,16 @@
 REM Adjusted path to TmplCodeGen
 set TOOL_DIR=..\..\..\Projects\TmplCodeGen
 
-%TOOL_DIR%\TmplCodeGen.exe System.List || goto :error
-%TOOL_DIR%\TmplCodeGen.exe System.Dictionary || goto :error
+%TOOL_DIR%\TmplCodeGen.exe Study.List || goto :error
+%TOOL_DIR%\TmplCodeGen.exe Study.Dictionary || goto :error
 
-%TOOL_DIR%\TmplCodeGen.exe include_partials Source\System.Collections.Factory.pas || goto :error
-%TOOL_DIR%\TmplCodeGen.exe include_partials Source\System.Collections.Interfaces.pas || goto :error
+%TOOL_DIR%\TmplCodeGen.exe include_partials Source\Study.Collections.Factory.pas || goto :error
+%TOOL_DIR%\TmplCodeGen.exe include_partials Source\Study.Collections.Interfaces.pas || goto :error
 
 REM Delete temp files
-del System.List.pas
-del System.Dictionary.pas
-del System.*.part.pas
+del Study.List.pas
+del Study.Dictionary.pas
+del Study.*.part.pas
 
 echo Generation Complete.
 goto :EOF
