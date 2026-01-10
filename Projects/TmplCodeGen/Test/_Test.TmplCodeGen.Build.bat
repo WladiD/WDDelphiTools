3a6270d578f7556ef3e11f3602be425333030bee
WladiD <furevest@gmail.com>
2026-01-10 15:08:20 +0100

    TmplCodeGen: Integrate tests into unified build system

---
+++ Projects/TmplCodeGen/Test/_Test.TmplCodeGen.Build.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 65001 > nul
+setlocal
+pushd %~dp0
+
+call ..\..\..\_BuildBase.bat "Test.TmplCodeGen.dproj" %1
+
+if %ERRORLEVEL% neq 0 (
+    popd
+    exit /b %ERRORLEVEL%
+)
+
+popd
+endlocal
