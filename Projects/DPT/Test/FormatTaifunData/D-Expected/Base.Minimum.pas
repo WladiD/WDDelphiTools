8c466e29924b65966505011e726f281a9d7ededd
WladiD <furevest@gmail.com>
2026-03-20 06:22:22 +0100

    DPT: Minimum case

---
+++ Projects/DPT/Test/FormatTaifunData/D-Expected/Base.Minimum.pas
@@ -0,0 +1,50 @@
+﻿// ======================================================================
+//
+// Base.Minimum - Kurzbeschreibung der Unit
+//
+// Autor: Name
+//
+// ======================================================================
+
+{$I Base.Define.pas}
+
+unit Base.Minimum;
+
+{ ======================================================================= }
+interface
+{ ======================================================================= }
+
+uses
+
+  System.Classes,
+
+  Base.Progress.Interfaces,
+
+  Base.UI.Progress.Form;
+
+{ ----------------------------------------------------------------------- }
+
+type
+
+  TFormAbort = class(TFormProgress)
+   public
+    constructor Create(AOwner: TComponent; AAbort: PBoolean);
+  end;
+
+{ ======================================================================= }
+implementation
+{ ======================================================================= }
+
+{ ======================================================================= }
+{ TFormAbort                                                              }
+{ ======================================================================= }
+
+constructor TFormAbort.Create(AOwner: TComponent; AAbort: PBoolean);
+begin
+  inherited Create(AOwner,AAbort,fpsSingleProgress,20);
+  BarVisible[1]:=false;
+end;
+
+{ ======================================================================= }
+
+end.
